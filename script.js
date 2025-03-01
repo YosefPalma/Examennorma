b0366ee4d7b6340b1667f27d5015ead96f0c7777
YosefPalma <yosefpapu6@gmail.com>
2025-02-28 19:42:41 -0600

    correccion

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 document.getElementById('countButton').addEventListener('click', function() {
     const text = document.getElementById('inputText').value.trim();
     
-    // Función para contar las palabras
     function contarPalabras(frase) {
       const palabras = frase.split(" ").filter(palabra => palabra.length > 0);
       return palabras.length;
     }
   
-    // Contar y mostrar el resultado
     const numPalabras = contarPalabras(text);
     document.getElementById('result').innerText = `Número de palabras: ${numPalabras}`;
   });
