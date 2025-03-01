dc13898e6cb8536cbf15f5b6b1963971bc75ed44
YosefPalma <yosefpapu6@gmail.com>
2025-02-28 19:52:32 -0600

    Ultimo comit ya con todo listo

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById('countButton').addEventListener('click', function() {
     const text = document.getElementById('inputText').value.trim();
     
-    function contarPalabras(frase) {
+    function Contador(frase) {
       const palabras = frase.split(" ").filter(palabra => palabra.length > 0);
       return palabras.length;
     }
   
-    const numPalabras = contarPalabras(text);
-    document.getElementById('result').innerText = `Número de palabras: ${numPalabras}`;
+    const Npalabras = Contador(text);
+    document.getElementById('result').innerText = `Número de palabras: ${Npalabras}`;
   });
   
